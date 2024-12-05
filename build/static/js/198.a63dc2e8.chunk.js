2d1bbd395d6b4d0f0257189017e73e1f15f24ee1
Michele <nocchimichele13@gmail.com>
2024-12-05 20:58:29 +0100

    Aggiungi file di configurazione Vercel, robots.txt e manifest.json. Aggiorna tailwind.config.js e package.json. Aggiungi nuovi file JavaScript e HTML per la build.

---
+++ build/static/js/198.a63dc2e8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfixed_expenses=self.webpackChunkfixed_expenses||[]).push([[198],{198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.a63dc2e8.chunk.js.map
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,jsx}"],
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
